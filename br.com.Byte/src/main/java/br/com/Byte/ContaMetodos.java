3a0e54adaee5e50b19d710110290513686a96079
Cleber <cleberbatista99@hotmail.com>
2023-01-31 17:00:23 -0300

    Adicionando metodo de Depositar

---
+++ br.com.Byte/src/main/java/br/com/Byte/ContaMetodos.java
@@ -0,0 +1,10 @@
+package br.com.Byte;
+
+public class ContaMetodos {
+ public static void main(String[] args) {
+	 Conta contaDoCleber = new Conta();
+	 contaDoCleber.saldo = 500 ;
+	 contaDoCleber.deposita(100);
+	 System.out.println(contaDoCleber.saldo);
+ }
+}
--- br.com.Byte/src/main/java/br/com/Byte/PrimeiraConta.java
+++ br.com.Byte/src/main/java/br/com/Byte/PrimeiraConta.java
@@ -3,9 +3,17 @@ package br.com.Byte;
 public class PrimeiraConta {
 
 	public static void main(String[] args) {
-		Conta PrimeiraConta = new Conta();
-		PrimeiraConta.saldo = 250;
-		System.out.println(PrimeiraConta.saldo);
-	}
-	
+		
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 250;
+		primeiraConta.saldo += 100 ;
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 500;
+		System.out.println(segundaConta.saldo);
+		
+		System.out.println("Primeira conta tem " + primeiraConta.saldo);
+		System.out.println("Segunda conta tem " + segundaConta.saldo);
+		
+	}	
 }
