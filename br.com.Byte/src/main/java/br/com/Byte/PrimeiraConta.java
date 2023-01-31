5911ebd52d7376733cf245ecd7b0e266c9a4b773
Cleber <cleberbatista99@hotmail.com>
2023-01-31 16:18:40 -0300

    Criando Especificação e instanciando

---
+++ br.com.Byte/src/main/java/br/com/Byte/PrimeiraConta.java
@@ -0,0 +1,11 @@
+package br.com.Byte;
+
+public class PrimeiraConta {
+
+	public static void main(String[] args) {
+		Conta PrimeiraConta = new Conta();
+		PrimeiraConta.saldo = 250;
+		System.out.println(PrimeiraConta.saldo);
+	}
+	
+}
